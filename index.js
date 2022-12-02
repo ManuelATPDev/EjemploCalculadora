e7b6c5ae6485a8df814dd82a8cc9bda482aef2a8
Manuel Torrealba <78187170+ManuelATPDev@users.noreply.github.com>
2022-12-02 17:08:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//suma,resta,multiplicacion,división
+
+function suma(){
+  let resultado = 5 + 2
+  console.log(resultado)
+}
+
+suma()
+
+function resta() {
+  let resultado = 5 - 2
+  console.log(resultado)
+}
+
+resta()
+
+function multiplicacion(){
+  let resultado = 5 * 2
+  console.log(resultado)
+}
+
+multiplicacion()
+
+function division() {
+  let resultado = 5 / 2
+  console.log(resultado)
+}
+
+division()
